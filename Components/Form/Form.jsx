eb3e775a3eab1e4ed60d942f42ad51007c2fff0c
Nada-123 <nada.alshakiry@gmail.com>
2024-03-15 16:04:49 +0300

    Done

--- Components/Form/Form.jsx
+++ Components/Form/Form.jsx
@@ -1,11 +1,40 @@
-export default function Form() {
-  return (
-    <form>
-      <h3>Search user Github</h3>
-      <div className="mb-3">
-        <label>User name</label>
-        <input type="text" className="form-control" placeholder="User Name" />
-      </div>
-    </form>
-  );
+import { useState } from "react";
+
+export default function Form({ setUserName }) {
+	const [inputText, setInputText] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setUserName(inputText);
+		setInputText("");
+	};
+
+	return (
+		<form
+			onSubmit={handleSubmit}
+			className='flex flex-col gap-5 pb-5'
+		>
+			<h1 className='text-center font-bold text-lg text-purple-900'>
+				Search user Github
+			</h1>
+			<div className='flex flex-col gap-2'>
+				<label className='font-semibold'>User name</label>
+				<input
+					type='text'
+					value={inputText}
+					onChange={(e) => {
+						setInputText(e.target.value);
+					}}
+					placeholder='Enter username'
+					className='border border-grey-200 rounded-md p-2'
+					required
+				/>
+				<div className='w-fill flex flex-col items-end'>
+					<button className=' bg-purple-900 p-2 rounded-lg text-white mt-auto w-1/2'>
+						Search
+					</button>
+				</div>
+			</div>
+		</form>
+	);
 }
--- app/page.js
+++ app/page.js
@@ -1,15 +1,41 @@
-import Form from '../Components/Form/Form'
-import UserCard from '../Components/UserCard/UserCard'
-import { Inter } from 'next/font/google'
+"use client";
+import { useEffect, useState } from "react";
+import Form from "../Components/Form/Form";
+import UserCard from "../Components/UserCard/UserCard";
+import { Inter } from "next/font/google";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div className="card">
-      <Form />
-      <UserCard />
-    </div>
-  );
+	const [input, setInput] = useState("");
+	const [userData, setUserData] = useState(null);
+
+	const fetchData = async () => {
+		const fetchURL = `https://api.github.com/users/${input}`;
+		const response = await fetch(fetchURL);
+		const newData = await response.json();
+		if (!input) return;
+		if (response.ok) {
+			setUserData(newData);
+		} else {
+			alert("User not found");
+			console.error("Invalid response structure:", newData);
+			setUserData("");
+		}
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, [input]);
+
+	const handleFormSubmit = (input) => {
+		setInput(input);
+	};
+
+	return (
+		<div className='h-fit w-80 bg-gray-50 rounded-xl p-3 shadow-md'>
+			<Form setUserName={handleFormSubmit} />
+			<UserCard userData={userData} />
+		</div>
+	);
 }
